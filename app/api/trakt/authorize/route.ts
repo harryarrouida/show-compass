1eaacec6d3d0ad40e73b0714f68a051cc7ccb495
harryarrouida <hamzaarrouida@gmail.com>
2025-01-14 03:20:47 +0100

    fix

--- app/api/trakt/authorize/route.ts
+++ app/api/trakt/authorize/route.ts
@@ -1,16 +1,18 @@
 import axios from 'axios';
 import { NextResponse, NextRequest } from 'next/server';
 
-const clientId = process.env.NEXT_PUBLIC_TRAKT_CLIENT_ID;
+const clientId = process.env.NEXT_PUBLIC_TRAKT_CLIENT_ID!;
 const clientSecret = process.env.NEXT_PUBLIC_TRAKT_CLIENT_SECRET;
-const baseUrl = 'https://api.trakt.tv';
+const baseUrl = process.env.NEXT_PUBLIC_TRAKT_BASE_URL!;
+
+const redirectUri = process.env.NEXT_PUBLIC_TRAKT_REDIRECT_URI!;
 
 export async function GET(request: NextRequest) { 
     const url = `${baseUrl}/oauth/authorize?` + new URLSearchParams({
         response_type: "code",
         client_id: clientId!,
         scope: "public", 
-        redirect_uri: "http://localhost:3000/trakt"
+        redirect_uri: redirectUri
     }).toString();
     
     return Response.json({ url });
